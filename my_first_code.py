cdce340349893ea3643ff30d4dfcd326aa545671
feinwc <102186429+feinwc@users.noreply.github.com>
2022-06-01 21:40:19 +0100

    my_first_code

---
+++ my_first_code.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+# this is my first line of code
+print("My Name is Nai-Wen, Chung.")
+print("The date is 1st June, 2022.")
+
+
+# In[8]:
+
+
+# Write a program to analyse real estate data
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+f = 12900
+
+total_sales = a + b + c + d + f
+print(total_sales)
+
+
+# In[9]:
+
+
+#calculate the average sales price for the month
+
+avg_sales = total_sales/5
+print(avg_sales)
+
+
+# In[11]:
+
+
+#find the missing value from the last column of the table.
+
+missing_value = c - 6500
+print(missing_value)
+
